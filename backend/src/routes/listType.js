8ef3e5985c86c097936e7e7d8674ee12cf5453fd
d0omed <43421102+d0omed@users.noreply.github.com>
2021-10-19 22:26:58 +0200

    feat(backend): add list type use cases and controller

--- backend/src/routes/listType.js
+++ backend/src/routes/listType.js
@@ -1,12 +1,63 @@
 const { Router } = require('express')
-const { get } = require('../use-cases/listType')
+const { getAll, getOne, add, remove, update } = require('../use-cases/listType')
 const router = Router()
 
-router.get('/', async (req, res, next) => {
-  const { limit, offset } = req.body
+router.get('/', async (req, res) => {
+  const { limit, offset, name } = req.query
   try {
-    const result = await get({ limit, offset })
+    const result = await getAll({ limit, offset, name })
     res.status(200).json(result)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const result = await getOne(id)
+    if (result) {
+      res.status(200).json(result)
+    } else {
+      res.status(404).json({ message: 'Not found.' })
+    }
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const added = await add(req.body)
+    res.status(201).json(added)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const [count, rows] = await update({ id, listType: req.body })
+    if (count > 0) {
+      res.status(200).json({ count, rows })
+    } else {
+      res.status(404).json({ message: 'Not found.' })
+    }
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const count = await remove(id)
+    if (count > 0) {
+      res.status(200).json({ count })
+    } else {
+      res.status(404).json({ message: 'Not found.' })
+    }
   } catch (err) {
     res.status(500).json(err)
   }
--- backend/src/use-cases/listType.js
+++ backend/src/use-cases/listType.js
@@ -1,7 +1,26 @@
+const { Op } = require('sequelize')
 const ListType = require('../models/ListType')
 
 module.exports = {
-  get({ limit = 10, offset = 0 }) {
-    return ListType.findAndCountAll({ limit, offset })
+  getAll({ limit = 10, offset = 0, name = '' }) {
+    return ListType.findAndCountAll({
+      limit, offset, where: {
+        name: {
+          [Op.like]: `${name}%`
+        }
+      }
+    })
+  },
+  getOne(id) {
+    return ListType.findByPk(id)
+  },
+  add(listType) {
+    return ListType.create(listType)
+  },
+  update({ id, listType }) {
+    return ListType.update(listType, { where: { id } })
+  },
+  remove(id) {
+    return ListType.destroy({ where: { id } })
   }
 }
