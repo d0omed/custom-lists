439b208b8e024ed45aa8550dec5353921b317c01
d0omed <43421102+d0omed@users.noreply.github.com>
2021-11-07 16:43:50 +0100

    fix(backend): add cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const sequelize = require('./database')
+const cors = require('cors')
 const routes = require('./routes')
 
 sequelize.sync().then(() => console.log('DB is ready !'))
 
 const app = express()
 
+app.use(cors())
+app.options('http://localhost:3001', cors())
 app.use(express.json())
 
 app.use(routes)
